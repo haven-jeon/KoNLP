9e59978932fa7f0519b499ca20ad6d624a101a03
haven-jeon <haven.jeon@nexr.com>
2012-08-30 17:27:00 +0900

    added tag datasets

---
+++ R/tagdata.R
@@ -0,0 +1,35 @@
+# Data Description File for roxygen2
+
+#' Keystroke misspell cost table
+#'
+#' @name editweights
+#' @docType data
+#' @author Heewon Jeon \email{madjakarta@@gmail..com}
+NULL
+
+
+#' KAIST tag to Sejong tag
+#'
+#' @name KtoS
+#' @docType data
+#' @author Heewon Jeon \email{madjakarta@@gmail..com}
+NULL
+
+
+#' Sejong tag to KAIST tag
+#'
+#' @name StoK
+#' @docType data
+#' @author Heewon Jeon \email{madjakarta@@gmail..com}
+NULL
+
+
+
+
+#' tag names
+#'
+#' @name tags
+#' @docType data
+#' @author Heewon Jeon \email{madjakarta@@gmail..com}
+NULL
+
