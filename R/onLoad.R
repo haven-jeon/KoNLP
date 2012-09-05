64caaa1130bbc15be831c7e1a5b5ab2809b49458
Heewon Jeon <haven.jeon@nexr.com>
2012-09-05 14:54:06 +0900

    Update R/onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -35,8 +35,8 @@
 
 .onAttach <- function(libname, pkgname){
   #uncompress Sejong Hannanum dictionary, if necessary
-  dicpath <- paste(system.file(package=.DicPkgName),"/dics/", sep="")
-  dics <- paste(dicpath,"handics.zip", sep="")
+  dicpath <- paste(system.file(package=.DicPkgName),"/dics", sep="")
+  dics <- paste(dicpath,"/handics.zip", sep="")
   if(file.exists(dics)){
     unzip(dics,exdir=dicpath,list=F, overwrite=T)
     file.remove(dics)
